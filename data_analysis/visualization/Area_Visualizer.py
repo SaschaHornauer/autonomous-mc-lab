d8dc297c522e07c8e6a3a63b1bd7ac0fba1fd3fb
Sascha Hornauer <saschaho@icsi.berkeley.edu>
2017-05-01 21:57:39 -0700

    backup

--- data_analysis/visualization/Area_Visualizer.py
+++ data_analysis/visualization/Area_Visualizer.py
@@ -19,11 +19,17 @@ class Marker_Position(object):
     __pos_xy = None
     __shift_xy = None
     __marker_id = None
+    __aquired_at_distance = None
     
-    def __init__(self,marker_id,pos_xy,shift_xy):
+    def __init__(self, marker_id, pos_xy, shift_xy, distance):
         __marker_id = marker_id
         __pos_xy = pos_xy
         __shift_xy = shift_xy
+        __aquired_at_distance = distance
+        
+    def get_distance(self):
+        return self.__aquired_at_distance
+        
     
 
 class Area_Visualizer(object):
@@ -39,23 +45,23 @@ class Area_Visualizer(object):
     def __init__(self):
         pass      
 
-    def visualize_markers_center_line(self,markers):
-        img2 = np.ones((600,600,3), np.uint8)
+    def visualize_markers_center_line(self, markers):
+        img2 = np.ones((600, 600, 3), np.uint8)
         
         # This factor is rather arbitrary. The x,y results of the following
         # calculation are approximately in between 0 and 8 and accordingly this
         # factor is chosen
-        scale_factor = 300.0 * (1.0/8.0)
+        scale_factor = 300.0 * (1.0 / 8.0)
         shift_factor = 300
-        turn_factor = np.pi/2.0 # division by int gives integer
+        turn_factor = np.pi / 2.0  # division by int gives integer
         
         # Reduce confidence for each marker in the persistent list, if there are any yet
         for marker_id in self.persistent_markers:
-            self.persistent_markers[marker_id].confidence = self.persistent_markers[marker_id].confidence-0.1
+            self.persistent_markers[marker_id].confidence = self.persistent_markers[marker_id].confidence - 0.1
         
         # Add markers to list, overwriting old markers and thereby increasing confidence levels
         for marker in markers:
-            self.persistent_markers[str(marker.id)]=marker
+            self.persistent_markers[str(marker.id)] = marker
         
         
         # Now draw lines onto new window
@@ -67,40 +73,40 @@ class Area_Visualizer(object):
             xy1 = marker.corners_xy_pos[0]
             xy2 = marker.corners_xy_pos[1]
 
-            cv2.line(img2,(xy1[0],xy1[1]+shift_factor),(xy2[0],xy2[1]+shift_factor),(0,0,255*marker.confidence),1)
+            cv2.line(img2, (xy1[0], xy1[1] + shift_factor), (xy2[0], xy2[1] + shift_factor), (0, 0, 255 * marker.confidence), 1)
 
             distance = marker.corners_distances_angles['distance']
             angle = marker.corners_distances_angles['angle']
             
             # Draw top view outline
-            x_a,y_a = cv2.polarToCart(distance,angle-turn_factor)
+            x_a, y_a = cv2.polarToCart(distance, angle - turn_factor)
             x_a[0] = x_a[0] * scale_factor + shift_factor
             y_a[0] = y_a[0] * scale_factor + shift_factor
-            cv2.circle(img2,(x_a[0],y_a[0]),2,(0,0,255*marker.confidence),-1)
+            cv2.circle(img2, (x_a[0], y_a[0]), 2, (0, 0, 255 * marker.confidence), -1)
                 
-            #ircle(img, center, radius, color[, thickness[, lineType[, shift]]]) 
+            # ircle(img, center, radius, color[, thickness[, lineType[, shift]]]) 
                 
             # Draw outer viewport lines
             
             fov = np.deg2rad(110.0)
                 
-            x_orig,y_orig = cv2.polarToCart(0.0,(-fov/2.0)-turn_factor)
-            x_dest,y_dest = cv2.polarToCart(8.0,(-fov/2.0)-turn_factor)
+            x_orig, y_orig = cv2.polarToCart(0.0, (-fov / 2.0) - turn_factor)
+            x_dest, y_dest = cv2.polarToCart(8.0, (-fov / 2.0) - turn_factor)
             x_orig[0] = x_orig[0] * scale_factor + shift_factor
             y_orig[0] = y_orig[0] * scale_factor + shift_factor
             x_dest[0] = x_dest[0] * scale_factor + shift_factor
             y_dest[0] = y_dest[0] * scale_factor + shift_factor
             
-            cv2.line(img2,(x_orig[0],y_orig[0]),(x_dest[0],y_dest[0]),(255,255,255),1)
+            cv2.line(img2, (x_orig[0], y_orig[0]), (x_dest[0], y_dest[0]), (255, 255, 255), 1)
             
-            x_orig_max,y_orig_max = cv2.polarToCart(0.0,(fov/2.0)-turn_factor)
-            x_dest_max,y_dest_max = cv2.polarToCart(8.0,(fov/2.0)-turn_factor)
+            x_orig_max, y_orig_max = cv2.polarToCart(0.0, (fov / 2.0) - turn_factor)
+            x_dest_max, y_dest_max = cv2.polarToCart(8.0, (fov / 2.0) - turn_factor)
             x_orig_max[0] = x_orig_max[0] * scale_factor + shift_factor
             y_orig_max[0] = y_orig_max[0] * scale_factor + shift_factor
             x_dest_max[0] = x_dest_max[0] * scale_factor + shift_factor
             y_dest_max[0] = y_dest_max[0] * scale_factor + shift_factor
             
-            cv2.line(img2,(x_orig_max[0],y_orig_max[0]),(x_dest_max[0],y_dest_max[0]),(255,255,255),1)
+            cv2.line(img2, (x_orig_max[0], y_orig_max[0]), (x_dest_max[0], y_dest_max[0]), (255, 255, 255), 1)
             
             
             
@@ -110,7 +116,7 @@ class Area_Visualizer(object):
             # put though this is highly experimental and here I have all
             # the needed information handily available
             
-            #for marker in markers:
+            # for marker in markers:
             #    self.map.addMarker(marker_id,distance,angle)
             
             # map test code
@@ -119,24 +125,23 @@ class Area_Visualizer(object):
             
             
             
-        cv2.imshow('topView',img2)
-        cv2.moveWindow('topView',700,0)
-    
-   
+        cv2.imshow('topView', img2)
+        cv2.moveWindow('topView', 700, 0)
     
     def show_top_view(self, markers):
         
-        cv_image = np.ones((600,600,3), np.uint8)
+        cv_image = np.ones((600, 600, 3), np.uint8)
         
         # Some arbitrary scaling parameters for visualisation are set
-        scale_factor = 300.0 * (1.0/8.0)
+        scale_factor = int(300.0 * (1.0 / 8.0))
         shift_factor = 300
-        turn_factor = np.pi/2.0 
+        turn_factor = np.pi / 2.0 
         current_marker_ids = []
+        current_xy = (0,0)
         
         # Reduce confidence for each marker in the persistent list, if there are any yet
         for marker_id in self.persistent_markers:
-            self.persistent_markers[marker_id].confidence = self.persistent_markers[marker_id].confidence-0.1
+            self.persistent_markers[marker_id].confidence = self.persistent_markers[marker_id].confidence - 0.1
         
         # Add markers to list, overwriting old markers and thereby recalculating confidence levels
         for marker in markers:
@@ -153,26 +158,46 @@ class Area_Visualizer(object):
                 self.base_marker_id = marker_id
                 self.base_marker_found = True
             
-                orig_xy = self.get_marker_xy(current_marker)
+                orig_xy, orig_distance, _= self.get_marker_xy(current_marker)
             
                 # We assume this is the basis of all future calculations
                 self.base_x = orig_xy[0]
                 self.base_y = orig_xy[1]
                 
                 # That base marker position is added with shift 0,0 because it is the reference
-                self.marker_positions[marker_id] = Marker_Position(marker_id,orig_xy,(0,0))
+                self.marker_positions[marker_id] = Marker_Position(marker_id, orig_xy, (0, 0), orig_distance)
             
             
-            # if the base marker is already found we check if the base marker is still visible
-            else:
+            elif marker_id != self.base_marker_id:
+                # if the base marker is already found and we are not right now looking
+                # at it, we check if the base marker is still visible
                 if self.base_marker_id in current_marker_ids:
                     # do shift calculations according to base marker
                     
-                    pass
+                    new_pos_xy, new_distance, _ = self.get_marker_xy(current_marker)
+                    shift_x = new_pos_xy[0] - self.base_x
+                    shift_y = new_pos_xy[1] - self.base_y
+                    new_marker_position = Marker_Position(marker_id, new_pos_xy, (shift_x, shift_y), new_distance)
+                    
+                    if marker_id in self.marker_positions.keys():
+                        # If the shift was already calculated then check at which distance and
+                        # update if the distance is smaller
+                        existing_position = self.marker_positions[marker_id]
+                        if existing_position.get_distance() > new_distance:
+                            self.marker_positions[marker_id] = new_marker_position
+                        
                 else:
                     # do more complicated calculations based on other shift values
                     pass
+            else:
+                # The base marker is already found and the marker_id is the self marker so we
+                # take the position directly
+                current_xy, _, _ = self.get_marker_xy(current_marker) 
                 
+            pos_x = current_xy[0]
+            pos_y = current_xy[1]
+            print (current_xy)
+            cv2.circle(cv_image, (int(scale_factor*pos_x+shift_factor),int(scale_factor*pos_y+shift_factor)),2,(0,0,255),2)
             
             # from now on, all future marker sightings of this marker mean a movement of the own vehicle
 #         elif marker_id in self.persistent_markers and not self.base_marker_found:
@@ -210,18 +235,18 @@ class Area_Visualizer(object):
 #             
 #             
 #             print self.get_marker_xy(marker)
-        #cv2.circle(cv_image, (scale_factor*marker_two_xy[0][0]+shift_factor,scale_factor*marker_two_xy[1][0]+shift_factor),2,(255,0,0),2)
+        # cv2.circle(cv_image, (scale_factor*marker_two_xy[0][0]+shift_factor,scale_factor*marker_two_xy[1][0]+shift_factor),2,(255,0,0),2)
         
-        #print(self.persistent_markers.keys())
-        cv2.imshow('topView',cv_image)
-        cv2.moveWindow('topView',700,0)
+        # print(self.persistent_markers.keys())
+        cv2.imshow('topView', cv_image)
+        cv2.moveWindow('topView', 700, 0)
 
-    def get_marker_xy(self,marker):
+    def get_marker_xy(self, marker):
         distance = aruco_data.get_distance(marker)
         angle = aruco_data.get_angle_surface(marker)
         # Now we can get the dx and dy of movement
         xy = cv2.polarToCart(distance, angle)
-        return tuple((xy[0][0][0],xy[1][0][0]))
+        return tuple((xy[0][0][0], xy[1][0][0])), distance, angle
 
 if __name__ == "__main__":
     visualizer = Area_Visualizer()
@@ -237,9 +262,9 @@ if __name__ == "__main__":
             markers = aruco_data.get_markers_in_image(cv_image)
             visualizer.show_top_view(markers)
         
-        cv2.imshow("video",cv_image)
+        cv2.imshow("video", cv_image)
         
-        key = cv2.waitKey(1000/30) & 0xFF
+        key = cv2.waitKey(1000 / 30) & 0xFF
         if key == ord('q'):
             break
         if key == ord(' '):
