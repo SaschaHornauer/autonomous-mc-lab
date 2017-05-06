24d2d6b80aced1faed60afc7336eb5d62f2518d7
Karl Zipser <karlzipser@berkeley.edu>
2017-05-05 18:05:30 -0700

    gacp

--- teg9/data/animate.py
+++ teg9/data/animate.py
@@ -1,6 +1,8 @@
 from kzpy3.vis import *
 
-def prepare_image(img,steer,motor,state,delay,scale,color_mode):
+
+
+def prepare_and_show_or_return_frame(img,steer,motor,state,delay,scale,color_mode,collision):
     
     bar_color = [0,0,0]
     if state == 1:
@@ -19,7 +21,8 @@ def prepare_image(img,steer,motor,state,delay,scale,color_mode):
         apply_rect_to_img(img,steer,0,99,bar_color,bar_color,0.9,0.1,center=True,reverse=True,horizontal=True)
     if motor != None:
         apply_rect_to_img(img,motor,0,99,bar_color,bar_color,0.9,0.1,center=True,reverse=True,horizontal=False)
-    
+    if collision == 1:
+        img[:10,:,:] = [255,0,0] 
      
     if delay == None:
         scale_img = cv2.resize(img, (0,0), fx=scale, fy=scale)
@@ -28,15 +31,18 @@ def prepare_image(img,steer,motor,state,delay,scale,color_mode):
         k = mci(img,delay,'animate',scale)
         return k  
 
-def animate(A):
+
+
+def animate_with_key_control(A):
+   
     timer = Timer(1)
     ctr = 0
     while True:
         while A['STOP_ANIMATOR_THREAD'] == False:
 
             if len(A['images']) < 2*30:
-                print 'waiting for images'
-                time.sleep(1)
+                print(d2s('Have',len(A['images']),'images, waiting...'))
+                time.sleep(2)
                 continue
             if timer.check():
                 print(d2s("A['current_img_index'] =", int(A['current_img_index'])))
@@ -69,11 +75,14 @@ def animate(A):
                     ctr += 1
                 continue
             else:
-                k = prepare_image(img,steer,None,state,A['delay'],A['scale'],A['color_mode'])
+                k = prepare_and_show_or_return_frame(img,steer,None,state,A['delay'],A['scale'],A['color_mode'],A['collisions'][int(A['current_img_index'])])
 
             if k == ord('q'):
-                print('Exiting animate_thread.animate')
+                print('Exiting animate_with_key_control')
+                A['STOP_GRAPH_THREAD'] = True
                 A['STOP_LOADER_THREAD'] = True
+                cv2.destroyAllWindows()
+                save_obj(A['collisions'],opjD(A['run_name']+'.collisions_'+time_str()))
                 return
             if k == ord(' '):
                 A['d_indx'] = 0
@@ -140,6 +149,24 @@ def animate(A):
             if k == ord('l'):
                 A['current_img_index'] += 2*30
 
+            if k == ord('j'):
+                while A['current_img_index'] < len(A['images']):
+                    if A['acc_xz_dst'][int(A['current_img_index'])]> 7.5:
+                        break
+                    A['current_img_index'] += 1
+
+            if k == ord('u'):
+                while A['current_img_index'] < len(A['images']):
+                    if A['loaded_collisions'][int(A['current_img_index'])]> 0:
+                        print('found collision')
+                        #pass
+                        #A['current_img_index'] -= 2*30
+                        break
+                    A['current_img_index'] += 1
+
+            if k == ord('h'):
+                if A['current_img_index'] > 12:
+                    A['collisions'][int(A['current_img_index'])-12] = 1 # reaction time
 
         time.sleep(0.2)
 
@@ -149,3 +176,95 @@ def animate(A):
 
 
 
+
+
+
+
+
+
+def graph(A):
+    figure('MSE')
+    indx = int(A['current_img_index'])
+    while True:
+        if A['STOP_GRAPH_THREAD'] == False:   
+            if len(A['images']) < 3*30:
+                print(d2s('Have',len(A['images']),'images, waiting...'))
+                pause(0.2)
+                continue
+            A_len = len(A['images'])
+            clf()       
+            steer = array(A['steer'])
+            plot(steer,'r.-')
+            plot([0,len(steer)],[49,49],'k')
+            t = indx#A['images'][indx][0]
+            xlim(t-30*5,t)
+            motor = array(A['motor'])
+            plot(motor,'b.-')
+            plot(10*A['loaded_collisions'])
+            """
+            N = 10
+
+            acc_x = array(A['acc_x'])
+            acc_y = array(A['acc_y'])
+            acc_z = array(A['acc_z'])
+            acc_x_smooth = 1.*acc_x
+            for i in range(N,len(acc_x)):
+                acc_x_smooth[i] = acc_x[i-N:i].mean()
+            acc_y_smooth = 1.*acc_x
+            for i in range(N,len(acc_y)):
+                acc_y_smooth[i] = acc_y[i-N:i].mean()
+            acc_z_smooth = 1.*acc_z
+            for i in range(N,len(acc_z)):
+                acc_z_smooth[i] = acc_z[i-N:i].mean()
+            acc_xyz = 1.*acc_x_smooth
+            acc_xyz[:] = sqrt(acc_x_smooth[:]**2+acc_z_smooth[:]**2)
+            plot(acc_x_smooth[:],'r')
+            plot(acc_y_smooth[:],'g')
+            plot(acc_z_smooth[:],'b')
+            plot(acc_xyz[:],'k')
+
+
+            gyro_x = array(A['gyro_x'])
+            gyro_y = array(A['gyro_y'])
+            gyro_z = array(A['gyro_z'])
+            gyro_x_smooth = 1.*gyro_x
+            for i in range(N,len(gyro_x)):
+                gyro_x_smooth[i] = gyro_x[i-N:i].mean()
+            gyro_y_smooth = 1.*gyro_x
+            for i in range(N,len(gyro_y)):
+                gyro_y_smooth[i] = gyro_y[i-N:i].mean()
+            gyro_z_smooth = 1.*gyro_z
+            for i in range(N,len(gyro_z)):
+                gyro_z_smooth[i] = gyro_z[i-N:i].mean()
+            gyro_xyz = 1.*gyro_x_smooth
+            gyro_xyz[:] = sqrt(gyro_x_smooth[:]**2+gyro_z_smooth[:]**2)
+            plot(gyro_x_smooth[:]/10.-25,'r')
+            plot(gyro_y_smooth[:]/10.-25,'g')
+            plot(gyro_z_smooth[:]/10.-25,'b')
+            #plot(gyro_xyz[:],'k')            
+
+
+            #multi_d = sqrt( (gyro_x/50.)**2 + (gyro_y/40.)**2 + (gyro_z/30.)**2 + ((acc_x-0.5)/2.5)**2 + ((acc_y-9.8)/5)**2 + ((acc_z-0.5)/2.5)**2 ) 
+            #plot(multi_d,'ko-')
+            """
+            plot(A['acc_xz_dst'])
+            ylim(-5,105)
+            if False: #hist_timer.check():
+                figure('left_deltas')
+                left_deltas = array(A['left_deltas'])
+                hist(left_deltas[:,1])
+                hist_timer.reset()
+                figure('MSE')
+            
+            pause(0.001)
+            while A_len == len(A['images']):
+                indx = int(A['current_img_index'])
+                t = indx
+                xlim(t-30*5,t)
+                if A['STOP_GRAPH_THREAD'] == True:
+                    print('Exiting graph')
+                    return
+                pause(0.2)
+        else:
+            print('Exiting graph')
+            return
--- teg9/data/animate_pkl_data.py
+++ teg9/data/animate_pkl_data.py
@@ -17,9 +17,7 @@ camera_sides = ['left','right']
 
 
 
-
-
-def multi_preprocess_bags(A,meta_path,bag_folder_path,bagfile_range=[]):
+def multi_preprocess_bagfiles(A,meta_path,bag_folder_path,bagfile_range=[]):
 
     A['meta'] = load_obj(meta_path)
 
@@ -27,28 +25,28 @@ def multi_preprocess_bags(A,meta_path,bag_folder_path,bagfile_range=[]):
     if len(bagfile_range) > 0:
         bag_files = bag_files[bagfile_range[0]:bagfile_range[1]]
     A['images'] = []
-    threading.Thread(target=multi_preprocess_thread,args=[A,bag_files]).start()
+    threading.Thread(target=multi_preprocess_bagfiles_thread,args=[A,bag_files]).start()
 
 
 
 
-def multi_preprocess_thread(A,bag_files):
+def multi_preprocess_bagfiles_thread(A,bag_files):
+    A['steer_previous'] = 49
+    A['motor_previous'] = 49
     for b in bag_files:
         if A['STOP_LOADER_THREAD']:
-            A['STOP_LOADER_THREAD'] = False
             print('Stopping multi_preprocess_thread.')
             break
-        preprocess(A,b)
+        preprocess_bagfiles(A,b)
 
 
 
 
-def preprocess(A,path):
+def preprocess_bagfiles(A,path):
     timer = Timer(0)
     
     for topic in image_topics + single_value_topics:
         if topic not in A:
-
             A[topic] = []
     for topic in vector3_topics:
         if topic+'_x' not in A:
@@ -56,15 +54,25 @@ def preprocess(A,path):
             A[topic+'_y'] = []
             A[topic+'_z'] = []
 
+    """
+    -2<acc_z<3,0.5
+    -2.5<acc_x<3,0.5
+    6<acc_y<13,9.8
+
+    -50 gyro_x 50
+    -40, gyro_y 40
+    -30, gyro_z 30
+    """
+
+
     if True:#try:
         cprint('Loading bagfile '+path,'yellow')
 
         bag = rosbag.Bag(path)
 
         color_mode = "rgb8"
-        steer_previous = 49
-        motor_previous = 49
-        for s in ['left']:#camera_sides:
+
+        for s in ['left']:
             for m in bag.read_messages(topics=['/bair_car/zed/'+s+'/image_rect_color']):
                 t = round(m.timestamp.to_time(),3)
                 if A['t_previous'] > 0:            
@@ -78,24 +86,19 @@ def preprocess(A,path):
                     print(d2s(t,"not in A['meta']"))
                 try:
                     if A['SMOOTHING']:
-                        A['steer'].append((A['meta'][t]['steer']+steer_previous)/2.0)
-                        A['motor'].append((A['meta'][t]['motor']+motor_previous)/2.0)
+                        A['steer'].append((A['meta'][t]['steer']+A['steer_previous'])/2.0)
+                        A['motor'].append((A['meta'][t]['motor']+A['motor_previous'])/2.0)
                         A['state'].append(A['meta'][t]['state'])
-                        steer_previous = A['steer'][-1]
-                        motor_previous = A['motor'][-1]
-                        #print steer_previous
+                        A['steer_previous'] = A['steer'][-1]
+                        A['motor_previous'] = A['motor'][-1]
                     else:
                         A['steer'].append(A['meta'][t]['steer'])
                         A['state'].append(A['meta'][t]['state'])
                         A['motor'].append(A['meta'][t]['motor'])
-                    #print A['steer']
                 except:
                     A['steer'].append(0)
                     A['state'].append(0)
                     A['motor'].append(0)
-
-
-
     #except Exception as e:
     #    print e.message, e.args
     print(d2s('Done in',timer.time(),'seconds'))
@@ -104,9 +107,16 @@ def preprocess(A,path):
 
 
 
-def multi_preprocess_pkl(A,meta_path,rgb_1to4_path):
+def multi_preprocess_pkl_files(A,meta_path,rgb_1to4_path):
+    for topic in image_topics + single_value_topics:
+        if topic not in A:
+            A[topic] = []
+    for topic in vector3_topics:
+        if topic+'_x' not in A:
+            A[topic+'_x'] = []
+            A[topic+'_y'] = []
+            A[topic+'_z'] = []
     A['meta'] = load_obj(opj(meta_path,'left_image_bound_to_data.pkl'))
-
     steer_previous = 49
     motor_previous = 49
     bag_pkls = sgg(opj(rgb_1to4_path,'*.bag.pkl'))
@@ -123,7 +133,6 @@ def multi_preprocess_pkl(A,meta_path,rgb_1to4_path):
                     A['state'].append(A['meta'][t]['state'])
                     steer_previous = A['steer'][-1]
                     motor_previous = A['motor'][-1]
-                    #print steer_previous
                 else:
                     A['steer'].append(A['meta'][t]['steer'])
                     A['state'].append(A['meta'][t]['state'])
@@ -132,7 +141,32 @@ def multi_preprocess_pkl(A,meta_path,rgb_1to4_path):
                 A['steer'].append(0)
                 A['state'].append(0)
                 A['motor'].append(0)
+            try:
+                A['acc_x'].append(A['meta'][t]['acc'][0])
+                A['acc_y'].append(A['meta'][t]['acc'][1])
+                A['acc_z'].append(A['meta'][t]['acc'][2])
 
+            except:
+                A['acc_x'].append(0)
+                A['acc_y'].append(1)
+                A['acc_z'].append(2)
+            try:
+                A['gyro_x'].append(A['meta'][t]['gyro'][0])
+                A['gyro_y'].append(A['meta'][t]['gyro'][1])
+                A['gyro_z'].append(A['meta'][t]['gyro'][2])
+
+            except:
+                A['gyro_x'].append(0)
+                A['gyro_y'].append(1)
+                A['gyro_z'].append(2)
+            try:
+                A['encoder'].append(A['meta'][t]['encoder'])
+
+            except:
+                A['encoder'].append(0)
+
+    A['acc_xz_dst'] = sqrt(array(A['acc_x'])**2 + array(A['acc_z'])**2)
+    A['collisions'] = 0*array(A['steer'])
 
 
 
@@ -148,8 +182,8 @@ def get_new_A(_=None):
     A['current_img_index'] = -A['d_indx']
     A['t_previous'] = 0
     A['left_deltas'] = []
-    A['scale'] = 1.0
-    A['delay'] = None#33
+    A['scale'] = 4.0
+    A['delay'] = 33
     A['steer'] = []
     A['state'] = []
     A['SMOOTHING'] = True
@@ -162,39 +196,96 @@ def get_new_A(_=None):
     return A
 
 
+#A = {}
+#bag_or_pkl = "pkl"  
+#data_path = opjD('bair_car_data_new')
+run_name = "direct_rewrite_test_25Apr17_12h40m27s_Mr_Black"
+
+def main():
+    A = {}
+    
+    bag_or_pkl = sys.argv[1]
+    if bag_or_pkl == 'bag':
+        print('Working with bag files')
+    elif bag_or_pkl == 'pkl':
+        print('Working with pkl file')
+    else:
+        print("sys.argv[1] must be 'bag' or 'pkl'.")
+        return
+    data_path = sys.argv[2]
+    if len(gg(data_path)) != 1:
+        print("if len(gg(meta_path)) != 1:")
+        return
+    run_name = sys.argv[3]
+
+    if len(sys.argv) > 4:
+        alt_bagfolder_path = sys.argv[4]
+    if len(sys.argv) > 5:
+        bagfile_range = sys.argv[5]
+    if len(sys.argv) > 6:
+        print("Too many arguments")
+        return
+    
+    
+
+    hist_timer = Timer(10)
+    
+    A = get_new_A(A)
+
+    A['run_name'] = run_name
+    A['loaded_collisions'] = None
+    collision_files = gg(opjD('collisions','*'))
+    for c in collision_files:
+        if run_name in c:
+            A['loaded_collisions'] = lo(c)
+            print 'loaded '+c
+            break
+
+    if bag_or_pkl == 'pkl':
+        meta_path = opj(data_path,'meta',run_name)
+        rgb_1to4_path = opj(data_path,'rgb_1to4',run_name)
+        multi_preprocess_pkl_files(A,meta_path,rgb_1to4_path)
+
+    elif bag_or_pkl == 'bag':
+        #meta_path = '/media/karlzipser/ExtraDrive3/from_Mr_Yellow/Mr_Yellow_Fern_11April2017/processed/caffe2_z2_color_direct_local_11Apr17_22h14m05s_Mr_Yellow/.preprocessed2/left_image_bound_to_data.pkl'
+        #path2 = '/media/karlzipser/ExtraDrive3/from_Mr_Yellow/Mr_Yellow_Fern_11April2017/processed/caffe2_z2_color_direct_local_11Apr17_22h14m05s_Mr_Yellow'
+        meta_path = opj(data_path,run_name,'.preprocessed2/left_image_bound_to_data.pkl')
+        bags_path = opj(data_path,run_name)
+        multi_preprocess_bagfiles(A,meta_path,bags_path,bagfile_range=[])
 
 
+
+    threading.Thread(target=animate.animate_with_key_control,args=[A]).start()
+    #threading.Thread(target=animate.graph,args=[A]).start()
+    animate.graph(A)
+    #raw_input()
+
+
+
+        
 
 
 if __name__ == '__main__':
-
-    hist_timer = Timer(10)
-    A = {}
-    A = get_new_A(A)
-
-    if False:
-        #meta_path = '/home/karlzipser/Desktop/bair_car_data_new/meta/caffe2_z2_color_direct_local_11Apr17_22h14m05s_Mr_Yellow'
-        #path2 =     '/home/karlzipser/Desktop/bair_car_data_new/rgb_1to4/caffe2_z2_color_direct_local_11Apr17_22h14m05s_Mr_Yellow'
-        #meta_path = '/home/karlzipser/Desktop/bair_car_data_new/meta/caffe2_z2_color_direct_local_01Jan13_00h01m07s_Mr_Yellow_A'
-        #path2 =     '/home/karlzipser/Desktop/bair_car_data_new/rgb_1to4/caffe2_z2_color_direct_local_01Jan13_00h01m07s_Mr_Yellow_A'
-        meta_path = '/home/karlzipser/Desktop/bair_car_data_new/meta/direct_rewrite_test_01May17_17h41m03s_Mr_Silver'
-        path2 =     '/home/karlzipser/Desktop/bair_car_data_new/rgb_1to4/direct_rewrite_test_01May17_17h41m03s_Mr_Silver'
-        
-        multi_preprocess_pkl(A,meta_path,path2)
-
-    if True:
-        #meta_path = '/media/karlzipser/ExtraDrive3/from_Mr_Yellow/Mr_Yellow_Fern_11April2017/processed/caffe2_z2_color_direct_local_11Apr17_22h14m05s_Mr_Yellow/.preprocessed2/left_image_bound_to_data.pkl'
-        #path2 = '/media/karlzipser/ExtraDrive3/from_Mr_Yellow/Mr_Yellow_Fern_11April2017/processed/caffe2_z2_color_direct_local_11Apr17_22h14m05s_Mr_Yellow'
-        meta_path = '/home/karlzipser/Desktop/one_bag/processed/direct_rewrite_test_01May17_17h41m03s_Mr_Silver/.preprocessed2/left_image_bound_to_data.pkl'
-        path2 = '/home/karlzipser/Desktop/one_bag/processed/direct_rewrite_test_01May17_17h41m03s_Mr_Silver'
-        multi_preprocess_bags(A,meta_path,path2,bagfile_range=[])
-
-
-
-    threading.Thread(target=animate.animate,args=[A]).start()
+    main()
 
 
 
 
+
+
+
+meta = sgg('/home/karlzipser/Desktop/bair_car_data_new/meta/*' )
+for i in range(len(meta)):
+
+    meta[i] = fname(meta[i])
+meta = set(meta)
+
+ctr = 0
+collisions = sgg('/home/karlzipser/Desktop/collisions/*' )
+for i in range(len(collisions)):
+    c = lo(collisions[i])
+    ctr += c.sum()
+    collisions[i] = fname(collisions[i]).split('.')[0]
+collisions = set(collisions)
 
 
