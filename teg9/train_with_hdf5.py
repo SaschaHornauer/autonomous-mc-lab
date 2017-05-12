2ed7e2ed38be0f66e8f2a917e77d916a870268e8
Karl Zipser <karlzipser@berkeley.edu>
2017-05-11 22:28:02 -0700

    gacp

--- teg9/train_with_hdf5.py
+++ teg9/train_with_hdf5.py
@@ -5,7 +5,7 @@ import caffe
 REPO = 'kzpy3'
 TEG = 'teg9'
 CAF = 'caf8'
-DISPLAY = True
+DISPLAY = False
 
 ignore = ['reject_run','left','out1_in2','Smyth','racing'] # runs with these labels are ignored
 require_one = [] # at least one of this type of run lable is required
