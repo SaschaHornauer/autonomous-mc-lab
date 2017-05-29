cd38a3e242dc06f6eca44601375e9f997e385761
Karl Zipser <karlzipser@hiMac.local>
2017-05-29 13:46:09 -0700

    gacp

--- scratch/y2017/temp2.py
+++ scratch/y2017/temp2.py
@@ -3,7 +3,64 @@
 
 
 
-def zp(d,range_lst,depth=0):#,max_depth_lst):
+def zlist_str(lst):
+	len_lst = len(lst)
+	lst_str = "["
+	for i in range(len_lst):
+		if i < 2:
+			lst_str += d2s(lst[i])
+		if i < 1:
+			lst_str += ','
+		else:
+			if len_lst > 2:
+				lst_str += "... "
+				break
+			else:
+				lst_str += "]"
+				return lst_str
+	if len_lst - 2 > 0:
+		for i in range(len_lst-2,len_lst):
+			lst_str += d2s(lst[i])
+			if i < len_lst-1:
+				lst_str += ','
+	lst_str += "]"
+	return lst_str
+
+
+
+def zlst_truncate(lst,show_ends=2):
+	if len(lst) > 2*show_ends:
+		out_lst = lst[:2] + ['...'] + lst[-2:]
+	else:
+		out_lst = lst
+	return out_lst
+
+def zlst_to_str(lst,truncate=True,decimal_places=2):
+	if truncate:
+		lst = zlst_truncate(lst)
+	lst_str = "["
+	for i in range(len(lst)):
+		e = lst[i]
+		if type(e) == str:
+			lst_str += e
+		elif type(e) == int:
+			lst_str += str(e)
+		elif is_number(e):
+			lst_str += str(dp(e,decimal_places))
+		elif type(e) == list:
+			lst_str += zlst_to_str(e)
+		else:
+			lst_str += '???'
+		if i < len(lst)-1:
+			lst_str += ' '
+	lst_str += ']'
+	return lst_str
+
+
+
+
+
+def zp(d,range_lst,depth=0):
 
 	sorted_keys = sorted(d.keys())
 	
@@ -17,8 +74,6 @@ def zp(d,range_lst,depth=0):#,max_depth_lst):
 			range_lst = range_lst + [-2]
 		else:
 			this_range = [this_range,this_range+1]
-
-	
 
 	if this_range[0] > 0:
 		cprint(d2n('\t'*depth,'0) ...'),'yellow')
@@ -34,7 +89,7 @@ def zp(d,range_lst,depth=0):#,max_depth_lst):
 		if isinstance(value,dict):
 			#if max_depth_lst[0] > depth:
 			if len(range_lst) > 1:
-				zp(value,range_lst[1:],depth=depth+1)#,max_depth_lst[1:])
+				zp(value,range_lst[1:],depth=depth+1)
 			else:
 				cprint(d2n('\t'*(depth+1),'...'),'yellow')
 		else:
