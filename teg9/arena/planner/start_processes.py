ef4c4cfee5300126de60e07f90b268a575b78266
Karl Zipser <karlzipser@berkeley.edu>
2017-06-16 15:58:04 -0700

    gacp

--- teg9/arena/planner/start_processes.py
+++ teg9/arena/planner/start_processes.py
@@ -1,5 +1,6 @@
 from kzpy3.utils2 import *
 
 
-for n in range(1):
-	unix(d2s("xterm -hold -e",opjh('kzpy3/teg9/arena/planner/top_controller.py'),n))
+for n in [5,6,7,8,9]:#[0,1,2,3,4]:
+	os.system(d2s("xterm -hold -e python",opjh('kzpy3/teg9/arena/planner/top_controller.py'),n,'&'))
+	pause(2)
